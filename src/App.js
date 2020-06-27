05c715852e4bf09b2b0256dceb893b360676d686
EralpO <eralponay10@gmail.com>
2020-06-27 21:34:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import {Fragment} from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {Navbar} from './components/layout/Navbar';
+import {Landing} from './components/layout/Landing';
+import {Register} from './components/auth/Register';
+import {Login} from './components/auth/Login'
+
 
 const  App = () => 
+<Router>
     <Fragment>
-     <h1>React App</h1>
+     <Navbar/>
+     <Route exact path="/" component={Landing}/>
+     <section className="container">
+         <Switch>
+         <Route exact path="/login" component={Login}/>
+         <Route exact path="/register" component={Register}/>
+         </Switch>
+     </section>
     </Fragment>
- 
+    </Router>
 
 export default App;
